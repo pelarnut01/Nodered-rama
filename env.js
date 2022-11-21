56c32ee342e0f408cd88629d7b681a39774bf6da
pelarnut01 <46145014+pelarnut01@users.noreply.github.com>
2022-11-21 11:46:15 +0700

    Create env.js

---
+++ env.js
@@ -0,0 +1,4 @@
+export const url = "http://192.168.1.81:8086";
+export const token = "P3q5pptgiZ2f07or9OQMJ6Vf6Tr-bWbXA14sWNKfnDNijXcMcw1VvoCXW_KKsDK8usNfZgYp-izG0xT6fr9jwA==";
+export const org = "Daikin";
+export const bucket = "rama";
